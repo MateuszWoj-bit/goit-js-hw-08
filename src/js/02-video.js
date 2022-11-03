fb8050f93ce1df689f4ca0e1044cdf79936ba293
Mateusz Wójcikowski <yankes90@gmail.com>
2022-11-03 20:02:33 +0100

    vid fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,4 +11,19 @@ function saveTime(currentTime) {
 }
 
 player.on('timeupdate', throttled);
-player.setCurrentTime(timeStamp);
+player
+  .setCurrentTime(timeStamp)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
